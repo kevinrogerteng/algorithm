6cac80dc0184f33522188f03400074ad45a113cc
Kevin Roger Teng <kevin.roger.teng@getchute.com>
2018-10-23 14:49:49 -0700

    use select instead

--- sum_in_array/sum_in_array.rb
+++ sum_in_array/sum_in_array.rb
@@ -1,6 +1,8 @@
 # Returms a pair of numbers equal to the sum
 module Algorithm
   def sum_in_array(numbers, sum)
-    numbers.combination(2).find_all { |x, y| x + y == sum } || []
+    # Array#Combination(int) returns a new 2 dimensional array of all possible combinations
+    # Array#Select returns a value if expression is true
+    numbers.combination(2).select { |x, y| x + y == sum } || []
   end
 end
